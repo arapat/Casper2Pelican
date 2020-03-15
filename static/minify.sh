7887d756495d59749e839f183ba8ad7c77a87f29
Julaiti Alafate <jalafate@gmail.com>
2020-03-15 13:59:57 -0700

    enabled menu

---
+++ static/minify.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+minify js/grid-gallery.js > js/grid-gallery.min.js
+minify js/index.js > js/index.min.js
+minify js/jquery.fitvids.js > js/jquery.fitvids.min.js
+minify css/custom.css > css/custom.min.css
+minify css/grid-gallery.css > css/grid-gallery.min.css
+minify css/highlight.css > css/highlight.min.css
+minify css/screen.css > css/screen.min.css
